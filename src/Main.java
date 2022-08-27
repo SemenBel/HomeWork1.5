56e25085e1fce51c50068a7a6f1b506d2b9edf7b
SemenBelousov <semen-belousov@bk.ru>
2022-08-27 19:30:55 +0500

    Прошу проерить ДЗ 1.5

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,71 @@
 public class Main {
     public static void main(String[] args) {
-
+        task3();
         }
+        public static void task1() {
+                int[] numbers = new int[3];
+                numbers[0] = 1;
+                int num1 = numbers[0];
+                numbers[1] = 2;
+                int num2 = numbers[1];
+                numbers[2] = 3;
+                int num3 = numbers[2];
 
+                double[] doub = {1.57, 7.654, 9.986};
+                double doub1 = 1.57;
+                double doub2 = 7.654;
+                double doub3 = 9.986;
 
+                long [] big = new long [2];
+                big[0] = 3452335345246L;
+                long big1 = big[0];
+                big[1] = 995464644245432662L;
+                long big2 = big[1];
+        }
+        public static void task2() {
+                int[] numbers = new int[3];
+                numbers[0] = 1;
+                int num1 = numbers[0];
+                numbers[1] = 2;
+                int num2 = numbers[1];
+                numbers[2] = 3;
+                int num3 = numbers[2];
+                System.out.println(num1 + ", " + num2 + ", " + num3);
+
+                double[] doub = {1.57, 7.654, 9.986};
+                double doub1 = 1.57;
+                double doub2 = 7.654;
+                double doub3 = 9.986;
+                System.out.println(doub1 + ", " + doub2 + ", " + doub3);
+
+                long [] big = new long [2];
+                big[0] = 3452335345246L;
+                long big1 = big[0];
+                big[1] = 995464644245432662L;
+                long big2 = big[1];
+                System.out.println(big1 + ", " + big2);
+        }
+        public static void task3() {
+                int[] numbers = new int[3];
+                numbers[0] = 1;
+                int num1 = numbers[0];
+                numbers[1] = 2;
+                int num2 = numbers[1];
+                numbers[2] = 3;
+                int num3 = numbers[2];
+                System.out.println(num3 + ", " + num2 + ", " + num1);
+
+                double[] doub = {1.57, 7.654, 9.986};
+                double doub1 = 1.57;
+                double doub2 = 7.654;
+                double doub3 = 9.986;
+                System.out.println(doub3 + ", " + doub2 + ", " + doub1);
+
+                long [] big = new long [2];
+                big[0] = 3452335345246L;
+                long big1 = big[0];
+                big[1] = 995464644245432662L;
+                long big2 = big[1];
+                System.out.println(big2 + ", " + big1);
+}
 }
